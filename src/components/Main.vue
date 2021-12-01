10e074b75c077ddee48d123fee1b5734053c8a12
Davide Tavella <tavella01@gmail.com>
2021-12-01 18:36:06 +0100

    milestone 1

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="main">
 
-     <p>{{researchFilmMain}}</p>
+   <div v-for="film in researchFilmMain"
+   :key="film.key"
+   class="card-film"> 
+
+      <h4>Titolo: {{film.title}}</h4>
+      <p>Titolo originale: {{film.original_title}}</p>
+      <p>Lingua: {{film.original_language}}</p>
+      <p>Voto: {{film.vote_average}}</p>
+      
+   </div>
 
   </div>
 </template>
@@ -27,9 +36,18 @@ export default {
    height: calc(100vh - 80px);
    width: 100vw;
    background: rgb(0,0,0);
-   background: linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(255,10,0,1) 100%);
+   background: linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(255,10,0,1) 110%);
    overflow-y: scroll;
    padding: 100px 10px;
 }
 
+p,
+h4{
+   color: white;
+}
+
+.card-film{
+   padding: 20px;
+   text-align: center;
+}
 </style>
