51db7986239a0f989dd39f8e870b20f6f3f10962
Davide Tavella <tavella01@gmail.com>
2021-12-01 15:33:18 +0100

    input and btn

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,6 +6,11 @@
       <h1>Boolflix</h1>
     </div>
 
+    <div class="input-header">
+      <input class="form-control" type="text" placeholder="Inserisci il nome di un film...">
+      <button type="button" class="button-input">Cerca</button>
+    </div>
+
   </div>
 </template>
 
@@ -25,23 +30,41 @@ export default {
   width: 100vw;
   background-color: black;
   font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
-.logo-header{
+.logo-header,
+.input-header{
   width: 20%;
   display: flex;
   line-height: 80px;
   padding: 10px;
-  cursor: pointer;
 }
+
+
 
 .logo-header img{
   width: 28%;
+  cursor: pointer;
 }
 
 h1{
   color: #F00800;
   padding-left: 20px;
+  cursor: pointer;
 }
 
+.button-input{
+  background-color: #F00800;
+  border: none;
+  padding: 5px;
+  color: white;
+  margin-left: 10px;
+}
+
+.button-input:active{
+  background-color: #b40802;
+}
 </style>
