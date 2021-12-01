eb48a715aef0d069fed9ee81113ce7f008e41e48
Davide Tavella <tavella01@gmail.com>
2021-12-01 15:24:02 +0100

    logo-boolflix

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="header">
-    
+
+    <div class="logo-header">
+      <img src="../assets/logo.png" alt="LogoVue">
+      <h1>Boolflix</h1>
+    </div>
+
   </div>
 </template>
 
@@ -19,7 +24,24 @@ export default {
   height: 80px;
   width: 100vw;
   background-color: black;
+  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+}
 
+.logo-header{
+  width: 20%;
+  display: flex;
+  line-height: 80px;
+  padding: 10px;
+  cursor: pointer;
+}
+
+.logo-header img{
+  width: 28%;
+}
+
+h1{
+  color: #F00800;
+  padding-left: 20px;
 }
 
 </style>
