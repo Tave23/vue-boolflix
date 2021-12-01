82f637302fc322d4e76023817a6f2742c5bbc1db
Davide Tavella <tavella01@gmail.com>
2021-12-01 15:16:48 +0100

    first-commit

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+.header{
+  height: 80px;
+  width: 100vw;
+  background-color: black;
+
+}
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main">
+     main
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+    
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+.main{
+   height: calc(100vh - 80px);
+   width: 100vw;
+   background: rgb(0,0,0);
+   background: linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(255,10,0,1) 100%);
+}
+
+</style>
