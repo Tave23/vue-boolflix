1cd9bc8473dc4ab6410984586489438298706da9
Davide Tavella <tavella01@gmail.com>
2021-12-02 16:11:36 +0100

    flip-card 1.0

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,83 +6,100 @@
       </div>
 
       <div class="container-film">
+         <div class="row flip-boxes">
+            <div 
+            v-for="film in researchFilmMain"
+            :key="film.key"
+            class="col-md-3 col-sm-4 col-8 flip-box">
+               <div class="front" style="background-image: url(https://images.pexels.com/photos/39811/pexels-photo-39811.jpeg?auto=compress&cs=tinysrgb&h=350);">
+               <div class="content">
+                  <h4>{{film.title}}</h4><br>
+                  <span class="click-for-more">
+                  </span>
+               </div>
+               </div>
+               <div class="back">
+               <div class="content">
+                  <span class="title">
+                     <p>Titolo: </p>
+                     <h2>{{film.title}}</h2>
+                  </span>
+                  <p class="original-title">
+                     Titolo originale: {{film.original_title}}
+                  </p>
+                  
+                  <!-- lingua dinamica -->
+                  <div v-if="film.original_language === 'it'" class="lingua">
+                     <p>Lingua: </p><img src="../assets/it.png" alt="Italia" class="img-lingua">
+                  </div>
 
-         <div v-for="film in researchFilmMain"
-         :key="film.key"
-         class="card-film"> 
+                  <div v-else-if="film.original_language === 'en'" class="lingua">
+                     <p>Lingua: </p><img src="../assets/en.png" alt="England" class="img-lingua">
+                  </div>
 
-            <span class="title">
-               <p>Titolo: </p>
-               <h2>{{film.title}}</h2>
-            </span>
-            <p>Titolo originale: {{film.original_title}}</p>
-            
-            <!-- lingua dinamica -->
-            <div v-if="film.original_language === 'it'" class="lingua">
-               <p>Lingua: </p><img src="../assets/it.png" alt="Italia" class="img-lingua">
+                  <div v-else>
+                     <p>Lingua: {{film.original_language}}</p>
+                  </div>
+                  <!-- !lingua dinamica -->
+
+                  <p>Voto: {{film.vote_average}}</p>
+               </div>
+               </div>
             </div>
-
-            <div v-else-if="film.original_language === 'en'" class="lingua">
-               <p>Lingua: </p><img src="../assets/en.png" alt="England" class="img-lingua">
-            </div>
-
-            <div v-else>
-               <p>Lingua: {{film.original_language}}</p>
-            </div>
-            <!-- !lingua dinamica -->
-
-            <p>Voto: {{film.vote_average}}</p>
-            
          </div>
-
       </div>
 
-      <div 
-      v-if="emptySerie"
-      class="serieTv">
+      <div class="categories">
+         <h2>Serie TV</h2>
+      </div>
 
-         <div class="categories">
-            <h2>Serie TV</h2>
-         </div>
-
-         <div class="container-film">
-
-            <div v-for="serie in researchSeriesMain"
+      <!-- CONTAINER SERIE TV -->
+      <div class="container-film">
+         <div class="row flip-boxes">
+            <div 
+            v-for="serie in researchSeriesMain"
             :key="serie.key"
-            class="card-film"> 
-
-               <span class="title">
-                  <p>Titolo: </p>
-                  <h2>{{serie.name}}</h2>
-               </span>
-               <p>Titolo originale: {{serie.original_name}}</p>
-
-               <!-- lingua dinamica -->
-               <div v-if="serie.original_language === 'it'" class="lingua">
-                  <p>Lingua: </p><img src="../assets/it.png" alt="Italia" class="img-lingua">
+            class="col-md-3 col-sm-4 col-8 flip-box">
+               <!-- FRONTE CARD -->
+               <div class="front" style="background-image: url(https://images.pexels.com/photos/39811/pexels-photo-39811.jpeg?auto=compress&cs=tinysrgb&h=350);">
+               <div class="content">
+                  <h4>{{serie.name}}</h4><br>
+                  <span class="click-for-more">
+                  </span>
                </div>
-
-               <div v-else-if="serie.original_language === 'en'" class="lingua">
-                  <p>Lingua: </p><img src="../assets/en.png" alt="England" class="img-lingua">
                </div>
+               <!-- RETRO CARD -->
+               <div class="back">
+               <div class="content">
+                  <span class="title">
+                     <p>Titolo: </p>
+                     <h2>{{serie.name}}</h2>
+                  </span>
+                  <p class="original-title">
+                     Titolo originale: {{serie.original_name}}
+                  </p>
+                  
+                  <!-- lingua dinamica -->
+                  <div v-if="serie.original_language === 'it'" class="lingua">
+                     <p>Lingua: </p><img src="../assets/it.png" alt="Italia" class="img-lingua">
+                  </div>
 
-               <div v-else>
-                  <p>Lingua: {{serie.original_language}}</p>
+                  <div v-else-if="serie.original_language === 'en'" class="lingua">
+                     <p>Lingua: </p><img src="../assets/en.png" alt="England" class="img-lingua">
+                  </div>
+
+                  <div v-else>
+                     <p>Lingua: {{serie.original_language}}</p>
+                  </div>
+                  <!-- !lingua dinamica -->
+
+                  <p>Voto: {{serie.vote_average}}</p>
                </div>
-              <!-- !lingua dinamica -->
-               <p>Voto: {{serie.vote_average}}</p>
-               
+               </div>
             </div>
-
          </div>
-      
       </div>
-
-      <div 
-      v-else
-      class="emptySerieTv">
-
-      </div>
+      <!-- //CONTAINER SERIE TV -->
 
    </div>
 </template>
@@ -97,12 +114,14 @@ export default {
       researchSeriesMain: Array
    },
    data(){
-         return{
-            // da cambiare in false!!!!!!!!*******+
-            emptyFilm: true, 
+      return{
+         // da cambiare in false!!!!!!!!*******+
+         emptyFilm: true, 
 
-            emptySerie: true,
-         }
+         emptySerie: true,
+
+         urlBasePoster: 'https://image.tmdb.org/t/p/'
+      }
    },
    mounted(){
 
@@ -133,7 +152,7 @@ export default {
    height: calc(100vh - 80px);
    width: 100vw;
    background: rgb(0,0,0);
-   background: linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(255,10,0,1) 110%);
+   background: linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(255,10,0,1) 100%);
    overflow-y: scroll;
    padding: 100px 10px;
    font-family: 'Trebuchet MS', sans-serif;
@@ -142,7 +161,7 @@ export default {
 .container-film{
    display: flex;
    overflow-x: auto;
-   width: 90%;
+   width: 90%; 
    margin: 0 auto;
    margin-bottom: 70px;
    min-height: 150px;
@@ -156,11 +175,13 @@ export default {
    margin-right: 10px;
    margin-top: 15px;
    margin-bottom: 10px;
+   background-color: rgb(59, 2, 0);
 }
 
 .categories{
    width: 90%;
    margin: 0 auto;
+   margin-bottom: 20px;
 }
 
 p,
@@ -171,17 +192,131 @@ h2{
 
 .lingua{
    display: flex;
-   justify-content: center;
+   justify-content: left;
+   align-items: center;
+   padding-bottom: 10px;
 }
 
 .img-lingua{
-   width: 25px;
-
+   width: 20px;
+   height: 15px;
 }
 
 .card-film,
 .title{
-   padding: 20px;
-   text-align: center;
+   padding: 0 20px;
+   text-align: left;
+}
+
+.original-title{
+   padding-bottom: 10px;
+}
+
+
+// flipcard
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+}
+.flip-boxes {
+  justify-content: center;
+  display: flex;
+}
+.flip-box {
+  display: flex;
+  align-content: stretch;
+  min-height: 300px;
+  min-width: 240px;
+  flex-wrap: wrap;
+  position: relative;
+  border: 10px solid transparent;
+  padding:0;
+  border-top: 0;  
+  -webkit-perspective: 1000;
+          perspective: 1000;
+  
+  &:hover {
+    .back {
+      transform: rotateY(0deg);
+      z-index: 10;
+    }
+    
+    .front {
+      transform: rotateY(180deg);
+      z-index: -1;
+    }
+    
+  }
+  
+  .back, .front {
+    position: relative;
+    background-color: #1b2d61;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    flex: 0 0 100%;
+    -webkit-transition: all 1.5s cubic-bezier(.5,1,.5,1);
+    transition: all 1.5s cubic-bezier(.5,1.3,.5,1.3);
+    transform-style: preserve-3d;
+    background-size: cover;
+    background-position: center;
+  }
+  
+  .back {
+    background-color: rgb(59, 2, 0);
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+    z-index: -1;
+    transform: rotateY(-180deg);
+    
+    &:after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 30px;
+      height: 30px;
+      border: 15px solid transparent;
+      border-bottom-color: white;
+      border-left-color: white;
+    }
+  }
+  
+  .front {
+    z-index: 10;
+    
+    .content {
+      font-size: 2rem;
+    }
+    
+    &:after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      width: 30px;
+      height: 30px;
+      border: 15px solid #cecece;
+      border-bottom-color: white;
+      border-right-color: white;
+    }
+  }
+  
+  .content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    -webkit-transform: translateZ(50px);
+            transform: translateZ(50px);
+    text-shadow: 0px 0px 2px black;
+  
+  }
 }
 </style>
